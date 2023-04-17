196f8b6d2ad03235c6f206f4f4fb97a212af1a4d
Bruno Malatesta <6malacabeza9@gmail.com>
2023-04-17 16:42:23 -0300

    ultimo commit 1642

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+//CARROUSEL
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -19,3 +20,47 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
 }
+
+//FORMULARIO CONTACTO
+
+const formulario = document.getElementById('formulario-contacto');
+const nombreInput = document.getElementById('nombre');
+const emailInput = document.getElementById('email');
+const mensajeInput = document.getElementById('mensaje');
+
+formulario.addEventListener('submit', (event) => {
+  // Verificar si los campos están completos
+  if (!nombreInput.value || !emailInput.value || !mensajeInput.value) {
+    // Evitar que se envíe el formulario
+    event.preventDefault();
+
+    // Mostrar mensaje de error al usuario
+    const errorMensaje = document.getElementById('error-mensaje');
+    errorMensaje.innerHTML = 'Por favor, completa todos los campos';
+  }
+});
+
+
+//FORMULARIO RECLAMO
+const formulario2 = document.getElementById('formulario-reclamo');
+const nombreInput2 = document.getElementById('nombre-reclamo');
+const emailInput2 = document.getElementById('email-reclamo');
+const mensajeInput2 = document.getElementById('mensaje-reclamo');
+
+
+formulario2.addEventListener('submit', (event) => {
+  // Verificar si los campos están completos
+  if (!nombreInput2.value || !emailInput2.value || !mensajeInput2.value) {
+    // Evitar que se envíe el formulario
+    event.preventDefault();
+
+    // Mostrar mensaje de error al usuario
+    const errorMensaje2 = document.getElementById('error-mensaje2');
+    errorMensaje2.innerHTML = 'Por favor, completa todos los campos';
+  }
+});
+
+
+
+
+
