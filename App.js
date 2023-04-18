0edda56fca2750323cac2d7166f095a3cf12b0e4
Bruno Malatesta <6malacabeza9@gmail.com>
2023-04-17 22:42:52 -0300

    ultimo commit original

--- App.js
+++ App.js
@@ -1,3 +1,22 @@
+//CONSUMO API
+const api = "https://api.quotable.io/random";
+
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const btn = document.getElementById("btnfrase");
+
+btn.addEventListener("click", getQuote);
+
+function getQuote() {
+  fetch(api)
+    .then((res) => res.json())
+    .then((data) => {
+      quote.innerHTML = `"${data.content}"`;
+      author.innerHTML = `- ${data.author}`;
+    });
+}
+
+
 //CARROUSEL
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -22,55 +41,134 @@ function showSlides(n) {
 }
 
 //FORMULARIO CONTACTO
+function submitForm(event) {
+  event.preventDefault(); // Evitar que se envíe el formulario automáticamente
+  
+  // Configuración de las reglas de validación
+  const constraints = {
+  nombre: {
+      presence: true
+  },
+  email: {
+      presence: true,
+      email: true
+  },
+  mensaje: {
+      presence: true
+  }
+  };
 
-const formulario = document.getElementById("formulario-contacto");
-formulario.addEventListener("submit", function(event) {
-  event.preventDefault(); // Prevenir el comportamiento predeterminado del envío del formulario
-  
-  const nombre = document.getElementById("nombre").value;
-  const email = document.getElementById("email").value;
-  const mensaje = document.getElementById("mensaje").value;
-  
-  if (nombre !== "" && email !== "" && mensaje !== "") {
+  // Obtener los valores de los campos del formulario
+  const nombre = document.getElementById('nombre').value;
+  const email = document.getElementById('email').value;
+  const mensaje = document.getElementById('mensaje').value;
+  const formData = { nombre, email, mensaje };
+
+  // Realizar la validación
+  const errors = validate(formData, constraints);
+
+  if (errors) {
+    // Mostrar errores si los hay
     Toastify({
 
-      text: "Mensaje Enviado",
+      text: "Email Invalido",
+
       style: {
-        background: "#3c7fd8",
+        background: "red",
       },
       duration: 3000
       
       }).showToast();
-    formulario.reset(); // Reiniciar el formulario
   } else {
-    document.getElementById("error-mensaje").textContent = "Por favor, complete todos los campos"; // Mostrar mensaje de error si los campos requeridos no están completos
+    // Enviar el formulario si no hay errores
+    Toastify({
+
+      text: "Mensaje Enviado",
+
+      style: {
+        background: "green",
+      },
+      duration: 3000
+      
+      }).showToast();
+    document.getElementById('contactForm').reset(); // Limpiar el formulario
   }
-});
+}
 
 
 
 //FORMULARIO RECLAMO
-const formulario2 = document.getElementById("formulario-reclamo");
-formulario2.addEventListener("submit", function(event) {
-  event.preventDefault(); // Prevenir el comportamiento predeterminado del envío del formulario
-  
-  const nombre = document.getElementById("nombre-reclamo").value;
-  const email = document.getElementById("email-reclamo").value;
-  const mensaje = document.getElementById("mensaje-reclamo").value;
-  
-  if (nombre !== "" && email !== "" && mensaje !== "") {
-    Toastify({
 
+// Definir reglas de validación
+const constraints = {
+  firstName: {
+    presence: true,
+    length: {
+      minimum: 1,
+      maximum: 50
+    }
+  },
+  lastName: {
+    presence: true,
+    length: {
+      minimum: 1,
+      maximum: 50
+    }
+  },
+  phoneNumber: {
+    presence: true,
+    format: {
+      pattern: '[0-9]{10,12}',
+      message: 'Debe contener entre 10 y 12 dígitos numéricos'
+    }
+  },
+  message: {
+    presence: true,
+    length: {
+      minimum: 1,
+      maximum: 100
+    }
+  }
+};
+
+
+// Obtener el formulario por su ID
+const form = document.getElementById('myForm');
+
+// Manejador de envío del formulario
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  // Validar el formulario
+  const validationResult = validate(form, constraints);
+
+  // Verificar si el formulario es válido
+  if (validationResult === undefined) {
+    // Obtener los valores del formulario
+    const firstName = form.elements.firstName.value;
+    const lastName = form.elements.lastName.value;
+    const phoneNumber = form.elements.phoneNumber.value;
+    const message = form.elements.message.value;
+
+    // Crear un objeto con los valores del formulario
+    const reclamo = {
+      firstName: firstName,
+      lastName: lastName,
+      phoneNumber: phoneNumber,
+      message: message
+    };
+
+    // Mostrar alerta con resultado del envío y valores del formulario
+    Toastify({
       text: "Mensaje Enviado",
       style: {
-        background: "#3c7fd8",
+        background: "green",
       },
       duration: 3000
-      
-      }).showToast();
-    formulario.reset(); // Reiniciar el formulario
-  } else {
-    document.getElementById("error-mensaje2").textContent = "Por favor, complete todos los campos"; // Mostrar mensaje de error si los campos requeridos no están completos
+    }).showToast();
+    console.log(reclamo)
+    // Reiniciar el formulario
+    form.reset();
   }
 });
 
